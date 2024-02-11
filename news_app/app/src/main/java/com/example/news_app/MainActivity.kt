b6013db632e23c45a5a9cf609cfb30af3bf48e5b
chungHee Han <chunghuihan9@gmail.com>
2024-02-11 22:26:56 +0900

    news_app ch.6

--- news_app/app/src/main/java/com/example/news_app/MainActivity.kt
+++ news_app/app/src/main/java/com/example/news_app/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.news_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.news_app.databinding.ActivityMainBinding
 
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import org.jsoup.Jsoup
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,7 +16,8 @@ import retrofit2.Retrofit
 import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var newsAdapter : NewsAdapter
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://news.google.com/")
         .addConverterFactory(
@@ -25,12 +29,85 @@ class MainActivity : AppCompatActivity() {
         ).build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        newsAdapter = NewsAdapter()
         val newsService = retrofit.create(NewsService::class.java)
-        newsService.mainFeed().enqueue(object : Callback<NewsRss>{
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = newsAdapter
+        }
+        binding.feedChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.feedChip.isChecked=true
+            newsService.mainFeed().submitList()
+        }
+        binding.politicsChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.politicsChip.isChecked=true
+            newsService.politicsNews().submitList()
+
+        }
+        binding.economyChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.economyChip.isChecked=true
+            newsService.economyNews().submitList()
+        }
+        binding.societyChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.societyChip.isChecked=true
+            newsService.societysNews().submitList()
+        }
+        binding.itChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.itChip.isChecked=true
+            newsService.itNews().submitList()
+        }
+        binding.sportChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.sportChip.isChecked=true
+            newsService.sportNews().submitList()
+        }
+
+
+    }
+    private fun Call<NewsRss>.submitList(){
+        enqueue(object : Callback<NewsRss>{
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
-              Log.d("retrofit", "MainActivity - onResponse() - called ${response.body()?.channel?.items}")
+                val list = response.body()?.channel?.items.orEmpty().trasform()
+                //               Log.d("hch", "MainActivity - onResponse() - called${list}")
+                newsAdapter.submitList(list)
+                list.forEachIndexed { index, news ->
+                    Log.d("hch", "MainActivity - onResponse() - called${news}")
+                    Thread{
+                        try {
+                            val jsoup = Jsoup.connect(news.link).get()
+
+                            Log.d("googleImg", "MainActivity - onResponse() - called ${jsoup}")
+                            val element = jsoup.select("meta[property^=og:]")
+//                            Log.d("googleImg", "MainActivity - onResponse() - called ${element}")
+                            val ogImageNode = element.find {node->
+                                node.attr("property") == "og:image"
+                            }
+//                            Log.d("googleImg", "MainActivity - onResponse() - called ${ogImageNode}")
+                            val imageUrl = ogImageNode?.attr("content")
+                            news.imageUrl =imageUrl
+//                            Log.d("googleImg", "MainActivity - onResponse() - called ${imageUrl}")
+                        }catch (e: Exception){
+                            e.printStackTrace()
+                        }
+
+                        runOnUiThread{
+                            newsAdapter.notifyItemChanged(index)
+                        }
+
+                    }.start()
+
+                }
+
+                Log.d("retrofit", "MainActivity - onResponse() - called ${response.body()?.channel?.items}")
+
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
---
+++ news_app/app/src/main/java/com/example/news_app/NewsAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.news_app
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.news_app.databinding.ItemNewsBinding
+
+class NewsAdapter: ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(val binding: ItemNewsBinding ) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item: NewsModel){
+            binding.titleTextView.text =item.title
+            Glide.with(binding.thumbnailImageView)
+                .load(item.imageUrl)
+                .into(binding.thumbnailImageView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemNewsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+    companion object {
+        val  diffUtil= object:DiffUtil.ItemCallback<NewsModel>(){
+            override fun areItemsTheSame(oldItem: NewsModel, newItem: NewsModel): Boolean {
+                return oldItem===newItem
+            }
+
+            override fun areContentsTheSame(oldItem: NewsModel, newItem: NewsModel): Boolean {
+               return oldItem == newItem
+            }
+
+        }
+    }
+}
