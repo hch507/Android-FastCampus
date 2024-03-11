02ed879fabf4a268b0f57359b30ac6cfc4013288
chungHee Han <chunghuihan9@gmail.com>
2024-03-11 23:22:44 +0900

    part1 ch4. 05

---
+++ medical_information_app/app/src/main/java/com/example/medical_information_app/InputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.medical_information_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
---
+++ medical_information_app/app/src/main/java/com/example/medical_information_app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.medical_information_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medical_information_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goInputActivityButton.setOnClickListener {
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra("intentMessage","응급 의료정보")
+            startActivity(intent)
+        }
+    }
+}
